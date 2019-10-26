618df767b964a6890518f01f44a023e6778d904f
Daniel Castro <castrodd@gmail.com>
2019-10-26 14:53:05 -0700

    Alternative error calculatation

--- chapter_1/1_7.scm
+++ chapter_1/1_7.scm
@@ -10,12 +10,24 @@
 (define (good-enough? guess x)
     (< (abs (- (square guess) x)) 0.001))
 
+(define (alt-good-enough? future-guess guess x)
+    (> (/ guess (abs (- guess future-guess))) 1000))
+
 (define (sqrt-iter guess x)
     (if (good-enough? guess x)
         guess
         (sqrt-iter (improve guess x) x)))
 
+(define (alt-sqrt-iter guess x)
+    (if (alt-good-enough? (improve guess x) guess x)
+        (improve guess x)
+        (alt-sqrt-iter (improve guess x) x)))
+
 (define (sqrt x) 
     (sqrt-iter 1.0 x))
 
-(square (sqrt 100))
+(define (alt-sqrt x)
+    (alt-sqrt-iter 1.0 x))
+
+(sqrt 0.001)
+(alt-sqrt 0.001)
