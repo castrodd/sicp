8eb18b8d0f8a7d8b633e9da813ba430ec5daf128
Daniel Castro <castrodd@gmail.com>
2019-11-03 17:01:51 -0800

    pascal's triangle

---
+++ chapter_1/1_12.scm
@@ -0,0 +1,9 @@
+(define (pascals-triangle row column)
+  (cond ((> column row) 0)
+        ((or (= column 1) 
+             (= column row)) 1)
+        ((or (= column 2) (= (- row column) 1)) (- row 1))
+        (else (+ (pascals-triangle (- row 1) (- column 1)) 
+                 (pascals-triangle (- row 1) column)))))
+
+(pascals-triangle 11 6)
