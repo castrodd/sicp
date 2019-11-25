84a352aecaca0f3fb239060f6618514f1c9dc589
Daniel Castro <castrodd@gmail.com>
2019-11-24 20:33:14 -0800

    squaring function

---
+++ chapter_2/2_21.scm
@@ -0,0 +1,17 @@
+(define (square-list items)
+    (if (null? items)
+        nil
+        (cons (* (car items) (car items)) (square-list (cdr items)))))
+
+(define (square-list items)
+    (map (lambda (x) (* x x)) items))
+
+(define (map proc items)
+    (if (null? items)
+        nil
+        (cons (proc (car items))
+              (map proc (cdr items)))))
+
+(define nil ())
+
+(square-list (list 1 2 3 4 5))
