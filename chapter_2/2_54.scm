b781d6a7405598ee7f0c7e9fc2ba23fe4ba0bbde
Daniel Castro <castrodd@gmail.com>
2020-01-02 20:52:39 -0800

    equal? implementation

---
+++ chapter_2/2_54.scm
@@ -0,0 +1,7 @@
+(define (equal? a b)
+    (cond ((eq? a b) #t)
+          ((and (pair? a) 
+                (pair? b) 
+                (equal? (car a) (car b))
+                (equal? (cdr a) (cdr b))))
+          (else #f)))
